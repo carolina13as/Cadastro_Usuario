d833ebe3e1bf95184e15487a003651e232fdc74b
Carolina Alves <carolinaalvesdesouza98@gmail.com>
2023-05-30 14:40:52 -0400

    Alteração na telinha e na mascaras

--- Cadastro Usuario/Form1.Designer.cs
+++ Cadastro Usuario/Form1.Designer.cs
@@ -52,9 +52,9 @@
             this.label1.BackColor = System.Drawing.SystemColors.Control;
             this.label1.Font = new System.Drawing.Font("Showcard Gothic", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.Teal;
-            this.label1.Location = new System.Drawing.Point(219, 9);
+            this.label1.Location = new System.Drawing.Point(250, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(341, 36);
+            this.label1.Size = new System.Drawing.Size(427, 46);
             this.label1.TabIndex = 0;
             this.label1.Text = "Cadastro de Usuario";
             // 
@@ -63,9 +63,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(100, 130);
+            this.label2.Location = new System.Drawing.Point(114, 173);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 17);
+            this.label2.Size = new System.Drawing.Size(63, 22);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nome:";
             // 
@@ -73,9 +73,9 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(100, 235);
+            this.label3.Location = new System.Drawing.Point(114, 313);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 17);
+            this.label3.Size = new System.Drawing.Size(65, 22);
             this.label3.TabIndex = 2;
             this.label3.Text = "Senha:";
             // 
@@ -83,9 +83,9 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(100, 200);
+            this.label4.Location = new System.Drawing.Point(114, 267);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(55, 17);
+            this.label4.Size = new System.Drawing.Size(69, 22);
             this.label4.TabIndex = 3;
             this.label4.Text = "E-mail:";
             // 
@@ -93,9 +93,9 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(100, 164);
+            this.label5.Location = new System.Drawing.Point(114, 219);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(42, 17);
+            this.label5.Size = new System.Drawing.Size(53, 22);
             this.label5.TabIndex = 4;
             this.label5.Text = "CPF:";
             // 
@@ -103,41 +103,45 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label6.Location = new System.Drawing.Point(444, 235);
+            this.label6.Location = new System.Drawing.Point(507, 313);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(91, 17);
+            this.label6.Size = new System.Drawing.Size(115, 22);
             this.label6.TabIndex = 5;
             this.label6.Text = "Conf. Senha:";
             // 
             // texto1
             // 
-            this.texto1.Location = new System.Drawing.Point(170, 131);
+            this.texto1.Location = new System.Drawing.Point(194, 175);
+            this.texto1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.texto1.Name = "texto1";
-            this.texto1.Size = new System.Drawing.Size(236, 23);
+            this.texto1.Size = new System.Drawing.Size(269, 27);
             this.texto1.TabIndex = 6;
             // 
             // texto6
             // 
-            this.texto6.Location = new System.Drawing.Point(534, 233);
+            this.texto6.Location = new System.Drawing.Point(622, 308);
+            this.texto6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.texto6.Name = "texto6";
             this.texto6.PasswordChar = '*';
-            this.texto6.Size = new System.Drawing.Size(195, 23);
+            this.texto6.Size = new System.Drawing.Size(222, 27);
             this.texto6.TabIndex = 7;
             // 
             // texto3
             // 
-            this.texto3.Location = new System.Drawing.Point(170, 194);
+            this.texto3.Location = new System.Drawing.Point(194, 259);
+            this.texto3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.texto3.Name = "texto3";
-            this.texto3.Size = new System.Drawing.Size(236, 23);
+            this.texto3.Size = new System.Drawing.Size(269, 27);
             this.texto3.TabIndex = 9;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button1.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(232, 325);
+            this.button1.Location = new System.Drawing.Point(250, 433);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 31);
             this.button1.TabIndex = 11;
             this.button1.Text = "Salvar";
             this.button1.UseVisualStyleBackColor = false;
@@ -147,9 +151,10 @@
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.button2.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(355, 325);
+            this.button2.Location = new System.Drawing.Point(406, 433);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(104, 31);
             this.button2.TabIndex = 12;
             this.button2.Text = "Cancelar";
             this.button2.UseVisualStyleBackColor = false;
@@ -159,34 +164,39 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label8.Location = new System.Drawing.Point(466, 194);
+            this.label8.Location = new System.Drawing.Point(533, 259);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(70, 17);
+            this.label8.Size = new System.Drawing.Size(84, 22);
             this.label8.TabIndex = 14;
             this.label8.Text = "Telefone:";
             // 
             // texto2
             // 
-            this.texto2.Location = new System.Drawing.Point(170, 164);
+            this.texto2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.texto2.Location = new System.Drawing.Point(194, 219);
+            this.texto2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.texto2.Mask = "000,000,000-00";
             this.texto2.Name = "texto2";
-            this.texto2.Size = new System.Drawing.Size(184, 23);
+            this.texto2.Size = new System.Drawing.Size(142, 34);
             this.texto2.TabIndex = 16;
             // 
             // mask_tel
             // 
-            this.mask_tel.Location = new System.Drawing.Point(534, 188);
-            this.mask_tel.Mask = "(99) 000-0000";
+            this.mask_tel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.mask_tel.Location = new System.Drawing.Point(622, 252);
+            this.mask_tel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.mask_tel.Mask = "(00) 000-0000";
             this.mask_tel.Name = "mask_tel";
-            this.mask_tel.Size = new System.Drawing.Size(184, 23);
+            this.mask_tel.Size = new System.Drawing.Size(129, 34);
             this.mask_tel.TabIndex = 17;
             // 
             // texto4
             // 
-            this.texto4.Location = new System.Drawing.Point(170, 229);
+            this.texto4.Location = new System.Drawing.Point(194, 305);
+            this.texto4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.texto4.Name = "texto4";
             this.texto4.PasswordChar = '*';
-            this.texto4.Size = new System.Drawing.Size(220, 23);
+            this.texto4.Size = new System.Drawing.Size(251, 27);
             this.texto4.TabIndex = 18;
             // 
             // txt1
@@ -194,18 +204,18 @@
             this.txt1.AutoSize = true;
             this.txt1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.txt1.ForeColor = System.Drawing.Color.Red;
-            this.txt1.Location = new System.Drawing.Point(588, 338);
+            this.txt1.Location = new System.Drawing.Point(672, 451);
             this.txt1.Name = "txt1";
-            this.txt1.Size = new System.Drawing.Size(41, 22);
+            this.txt1.Size = new System.Drawing.Size(52, 26);
             this.txt1.TabIndex = 19;
             this.txt1.Text = "txt1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(740, 395);
+            this.ClientSize = new System.Drawing.Size(846, 527);
             this.Controls.Add(this.txt1);
             this.Controls.Add(this.texto4);
             this.Controls.Add(this.mask_tel);
@@ -222,6 +232,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Cadastro";
             this.ResumeLayout(false);
